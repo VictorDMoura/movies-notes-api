d5e4a8af5b4cd7aa7bd130f9201ec26a406c34b8
VictorDMoura <mouramvictor@gmail.com>
2023-12-03 15:30:56 -0300

    feat: create update users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -24,7 +24,7 @@ class UserController {
       throw new AppError("Please enter a valid email");
     }
 
-    const checkUserExists = await knex("users").where("email", email);
+    const [checkUserExists] = await knex("users").where("email", email);
     if (checkUserExists) {
       throw new AppError("User already exists!");
     }
@@ -40,6 +40,64 @@ class UserController {
     });
 
     res.status(201).json();
+  }
+
+  async update(req, res) {
+    const { name, email, avatar, password, old_password } = req.body;
+    const { id } = req.params;
+    const validEmail =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    const [user] = await knex("users").where("id", id);
+
+    if (!user) {
+      throw new AppError("User doesn't exist!");
+    }
+
+    if (email && !validEmail.test(email)) {
+      throw new AppError("Invalid email!");
+    }
+
+    const [userWithUpdatedEmail] = email
+      ? await knex("users").where("email", email)
+      : [];
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id != id) {
+      throw new AppError("Email is already in use ");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+      if (!checkOldPassword) {
+        throw new AppError("Old password doesn't match");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    // format date
+    const date = new Date();
+    const updated_at = `${date.getFullYear()}-${date.getMonth() + 1}-${date
+      .getDate()
+      .toString()
+      .padStart(
+        2,
+        "0"
+      )} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+    await knex("users")
+      .update({
+        name: user.name,
+        password: user.password,
+        email,
+        avatar,
+        updated_at,
+      })
+      .where("id", id);
+    res.status(200).json();
   }
 }
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,5 +5,6 @@ const userController = new UserController();
 const userRoute = Router();
 
 userRoute.post("/", userController.create);
+userRoute.put("/:id", userController.update);
 
 module.exports = userRoute;
