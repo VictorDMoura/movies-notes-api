f4038eafb2fab1e54eb27a7956320330e2f561b4
VictorDMoura <mouramvictor@gmail.com>
2023-12-03 13:19:56 -0300

    feat: addd apperror

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,8 +1,20 @@
 const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
 
 class UserController {
   async create(req, res) {
     const { name, email, password, avatar } = req.body;
+
+    if (!name || !email || !password) {
+      const requireField = !name
+        ? "Name"
+        : !email
+        ? "Email"
+        : !password
+        ? "Password"
+        : "";
+      throw new AppError(`${requireField} is a required field`);
+    }
 
     await knex("users").insert({
       name,
