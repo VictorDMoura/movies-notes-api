4bf3c53297ee59c7ea1fcf9de9a8b2ade8733f3a
VictorDMoura <mouramvictor@gmail.com>
2023-12-05 09:52:22 -0300

    feat: create tags route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const userRoute = require("./user.route");
 const movieNotesRoute = require("./movies_notes.route");
+const tagsRoute = require("./tags.route");
 
 const routes = Router();
 
 routes.use("/users", userRoute);
 routes.use("/movies-notes", movieNotesRoute);
+routes.use("/tags", tagsRoute);
 module.exports = routes;
